93320bac0db8059377106bb4992b2788324b5730
ebiiim <mail@ebiiim.com>
2020-06-06 03:55:14 +0900

    sort

---
+++ sort.go
@@ -0,0 +1,111 @@
+// Package bitonic implements a bitonic sorter.
+package bitonic
+
+import (
+	"sync"
+)
+
+// SortOrder represents sort order.
+type SortOrder bool
+
+const (
+	// Ascending represents ascending order.
+	Ascending = true
+	// Descending represents descending order.
+	Descending = false
+)
+
+// SortInts1 sorts `x` by `ord` order (non-concurrent).
+// `len(x)` must be a power of 2.
+func SortInts1(x []int, ord SortOrder) {
+	bitonicSort1(x, ord, len(x))
+}
+
+// SortInts sorts `x` by `ord` order (concurrent).
+// `len(x)` must be a power of 2.
+func SortInts(x []int, ord SortOrder) {
+	bitonicSort(x, ord, len(x))
+}
+
+// Threshold is used to decide whether to run concurrently.
+const Threshold = 1 << 14
+
+func bitonicSort(x []int, ord SortOrder, ln int) {
+	if ln <= 1 {
+		return
+	}
+	mid := ln >> 1
+	if mid >= Threshold {
+		var wg sync.WaitGroup
+		wg.Add(2)
+		go func() {
+			bitonicSort(x[:mid], true, mid)
+			wg.Done()
+		}()
+		go func() {
+			bitonicSort(x[mid:], false, mid)
+			wg.Done()
+		}()
+		wg.Wait()
+	} else {
+		bitonicSort(x[:mid], true, mid)
+		bitonicSort(x[mid:], false, mid)
+	}
+	bitonicMerge(x, ord, ln)
+}
+
+func bitonicMerge(x []int, ord SortOrder, ln int) {
+	if ln <= 1 {
+		return
+	}
+	compareAndSwap(x, ord, ln)
+	mid := ln >> 1
+	if mid >= Threshold {
+		var wg sync.WaitGroup
+		wg.Add(2)
+		go func() {
+			bitonicMerge(x[:mid], ord, mid)
+			wg.Done()
+		}()
+		go func() {
+			bitonicMerge(x[mid:], ord, mid)
+			wg.Done()
+		}()
+		wg.Wait()
+	} else {
+		bitonicMerge(x[:mid], ord, mid)
+		bitonicMerge(x[mid:], ord, mid)
+	}
+}
+
+func bitonicSort1(x []int, ord SortOrder, ln int) {
+	if ln <= 1 {
+		return
+	}
+	mid := ln >> 1
+	bitonicSort1(x[:mid], true, mid)
+	bitonicSort1(x[mid:], false, mid)
+	bitonicMerge1(x, ord, ln)
+}
+
+func bitonicMerge1(x []int, ord SortOrder, ln int) {
+	if ln <= 1 {
+		return
+	}
+	compareAndSwap(x, ord, ln)
+	mid := ln >> 1
+	bitonicMerge1(x[:mid], ord, mid)
+	bitonicMerge1(x[mid:], ord, mid)
+}
+
+func compareAndSwap(x []int, ord SortOrder, ln int) {
+	mid := ln >> 1
+	for i := 0; i < mid; i++ {
+		peer := mid ^ i
+		if (x[i] > x[peer]) == ord {
+			x[i] = x[i] ^ x[peer]
+			x[peer] = x[i] ^ x[peer]
+			x[i] = x[i] ^ x[peer]
+		}
+	}
+}
---
+++ sort_test.go
@@ -0,0 +1,121 @@
+package bitonic_test
+
+import (
+	"math/rand"
+	"os"
+	"reflect"
+	sort "sort"
+	"strconv"
+	"testing"
+
+	"github.com/ebiiim/bitonic"
+)
+
+func makeInts(ln int, ord bitonic.SortOrder) (ints, sortedInts []int) {
+	rand.Seed(12345)
+	ints = make([]int, ln)
+	sortedInts = make([]int, ln)
+	for i := 0; i < ln; i++ {
+		v := int(rand.Int31())
+		ints[i] = v
+		sortedInts[i] = v
+	}
+	if ord {
+		sort.Ints(sortedInts)
+	} else {
+		sort.Sort(sort.Reverse(sort.IntSlice(sortedInts)))
+	}
+	return ints, sortedInts
+}
+
+func TestSortInt(t *testing.T) {
+	var (
+		a1, a2 = makeInts(bitonic.Threshold<<1, bitonic.Ascending)
+		d1, d2 = makeInts(bitonic.Threshold<<1, bitonic.Descending)
+	)
+	cases := []struct {
+		name string
+		ord  bitonic.SortOrder
+		in   []int
+		want []int
+	}{
+		{"zero", bitonic.Ascending, []int{}, []int{}},
+		{"asc", bitonic.Ascending, []int{55, 44, 66, 88, 22, 11, 33, 77}, []int{11, 22, 33, 44, 55, 66, 77, 88}},
+		{"desc", bitonic.Descending, []int{55, 44, 66, 88, 22, 11, 33, 77}, []int{88, 77, 66, 55, 44, 33, 22, 11}},
+		{"asc_long", bitonic.Ascending, a1, a2},
+		{"desc_long", bitonic.Descending, d1, d2},
+	}
+	for _, c := range cases {
+		c := c
+		t.Run(c.name, func(t *testing.T) {
+			t.Parallel()
+			bitonic.SortInts(c.in, c.ord)
+			if !reflect.DeepEqual(c.in, c.want) {
+				t.Error("not equal")
+			}
+		})
+	}
+}
+
+func TestSortInt1(t *testing.T) {
+	var (
+		a1, a2 = makeInts(bitonic.Threshold<<1, bitonic.Ascending)
+		d1, d2 = makeInts(bitonic.Threshold<<1, bitonic.Descending)
+	)
+	cases := []struct {
+		name string
+		ord  bitonic.SortOrder
+		in   []int
+		want []int
+	}{
+		{"zero", bitonic.Ascending, []int{}, []int{}},
+		{"asc", bitonic.Ascending, []int{55, 44, 66, 88, 22, 11, 33, 77}, []int{11, 22, 33, 44, 55, 66, 77, 88}},
+		{"desc", bitonic.Descending, []int{55, 44, 66, 88, 22, 11, 33, 77}, []int{88, 77, 66, 55, 44, 33, 22, 11}},
+		{"asc_long", bitonic.Ascending, a1, a2},
+		{"desc_long", bitonic.Descending, d1, d2},
+	}
+	for _, c := range cases {
+		c := c
+		t.Run(c.name, func(t *testing.T) {
+			t.Parallel()
+			bitonic.SortInts1(c.in, c.ord)
+			if !reflect.DeepEqual(c.in, c.want) {
+				t.Error("not equal")
+			}
+		})
+	}
+}
+
+var benchSize = func() int {
+	if b, err := strconv.Atoi(os.Getenv("BITS")); err == nil {
+		return b
+	}
+	return 26 // default value
+}()
+
+func BenchmarkSortInt(b *testing.B) {
+	b.Logf("bitonic.SortInts %d integers", 1<<benchSize)
+	x, _ := makeInts(1<<benchSize, bitonic.Ascending)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		bitonic.SortInts(x, bitonic.Ascending)
+	}
+}
+
+func BenchmarkSortInt1(b *testing.B) {
+	b.Logf("bitonic.SortInts1 %d integers", 1<<benchSize)
+	x, _ := makeInts(1<<benchSize, bitonic.Ascending)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		bitonic.SortInts1(x, bitonic.Ascending)
+	}
+}
+
+func BenchmarkGoSort(b *testing.B) {
+	b.Logf("sort.Sort %d integers", 1<<benchSize)
+	x, _ := makeInts(1<<benchSize, bitonic.Ascending)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		sort.Ints(x)
+	}
+}
